ef39f07da8cc731a7379cd99250aaa48d314fc3b
mateus <mateus.oliveira@essentiagroup.com.br>
2025-02-28 14:06:54 -0300

    fix(condicional) - ajustando condicional de patos errados

--- patinhos.go
+++ patinhos.go
@@ -19,10 +19,11 @@ func Patinhos(patinhos int){
 			fmt.Printf("%d%s%d%s", patinhos, frase1, patinhosQueVoltaramDeLa, frase2)
 		}
 
-		if patinhosQueVoltaramDeLa == 1{
+		if patinhos == 1{
+			frase1 = " patinho foi passear,\n além das montanhas para brincar.\n A mamãe gritou 'Quack Quack Quack Quack!',\n mas nenhum"
 			frase2 = " patinho voltou de lá.\n\n\n"
 			var patinhoQueVoltouDeLa string;
-			frase1 = " patinho foi passear,\n além das montanhas para brincar.\n A mamãe gritou 'Quack Quack Quack Quack!',\n mas nenhum"
+			
 			fmt.Printf("%d%s%s%s", patinhos, frase1, patinhoQueVoltouDeLa, frase2)
 		}
 	} 
